0fcccfdc880c5d4e5d24cf471e39d11030904f32
m-tartari <michele.tartari@iit.it>
2020-09-22 17:25:47 +0200

    added automatic execution of phase-calib-free

--- phase-postprocess.py
+++ phase-postprocess.py
@@ -8,14 +8,23 @@ from matplotlib import pyplot as plt
 #import costum
 import plot_utils
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 def phase_postprocess(plot_all=False):
-    #read data from latest file --------------------------------------------------------------
+    # read data from latest file --------------------------------------------------------------
     list_of_files = glob.glob('/logs/*.log')
     file = max(list_of_files, key=os.path.getctime)
-    print('reading file: '+file)
+    print(bcolors.OKBLUE + '[i] Reading file: ' + file + bcolors.ENDC)
 
-    print('Processing started')
+    print(bcolors.OKBLUE + '[i] Post-processing ph_angle data' + bcolors.ENDC)
     # '%u64\t%u\t%u\t%u\t%u\t%f\t%f\t%d\t%f\t%f\t%f'
     ns = [np.uint64(x.split('\t')[0]) for x in open(file).readlines()]
     curr_type = [np.uint32(x.split('\t')[1]) for x in open(file).readlines()]
@@ -29,45 +38,53 @@ def phase_postprocess(plot_all=False):
     link_pos = [float(x.split('\t')[9]) for x in open(file).readlines()]
     aux_var = [float(x.split('\t')[10]) for x in open(file).readlines()]
 
-    #find where we start testing id instead of iq
-    cc = len(curr_type)-2
+    # find where we start testing id instead of iq
+    cc = len(curr_type) - 2
     for i in range(1, len(curr_type)):
         if (curr_type[i] != curr_type[i - 1]):
             # disp('found change of current curr_type')
             cc = i
             break
 
-    #Plot full test --------------------------------------------------------------
+    # Plot full test --------------------------------------------------------------
     if plot_all:
         fig, axs = plt.subplots(2)
         fig.suptitle('Velocity by current type')
 
-        axs[0].plot(ns[:cc], motor_vel[:cc], label='Motor Vel', color='b', marker='.')
+        axs[0].plot(ns[:cc],
+                    motor_vel[:cc],
+                    label='Motor Vel',
+                    color='b',
+                    marker='.')
         for i in range(1, cc):
-            if (loop_cnt[i] > loop_cnt[i-1]):
-                axs[0].axvline(ns[i-1], linestyle='--', color='r')
-            elif (step_cnt[i] > step_cnt[i-1]):
-                axs[0].axvline(ns[i-1], linestyle='--', color='g')
+            if (loop_cnt[i] > loop_cnt[i - 1]):
+                axs[0].axvline(ns[i - 1], linestyle='--', color='r')
+            elif (step_cnt[i] > step_cnt[i - 1]):
+                axs[0].axvline(ns[i - 1], linestyle='--', color='g')
         axs[0].set_title('i_q=  2.5A @ 1.2Hz')
         axs[0].set_ylabel('motor_vel (rad/s)')
         axs[0].set_xlabel('timestamp (ns)')
         axs[0].grid(b=True, which='major', axis='y', linestyle='-')
         axs[0].grid(b=True, which='minor', axis='y', linestyle=':')
-        axs[0].yaxis.set_major_locator(plt.MultipleLocator(
-            (max(motor_vel) - min(motor_vel))*1.1 / 4))
-        axs[0].yaxis.set_minor_locator(plt.MultipleLocator(
-            (max(motor_vel) - min(motor_vel))*1.1 / 12))
+        axs[0].yaxis.set_major_locator(
+            plt.MultipleLocator((max(motor_vel) - min(motor_vel)) * 1.1 / 4))
+        axs[0].yaxis.set_minor_locator(
+            plt.MultipleLocator((max(motor_vel) - min(motor_vel)) * 1.1 / 12))
         axs[0].spines['top'].set_visible(False)
         axs[0].spines['right'].set_visible(False)
         axs[0].spines['left'].set_visible(False)
 
-        axs[1].plot(ns[cc:], motor_vel[cc:], label='Motor Vel', color='b', marker='.')
+        axs[1].plot(ns[cc:],
+                    motor_vel[cc:],
+                    label='Motor Vel',
+                    color='b',
+                    marker='.')
         for i in range(cc, len(ns)):
-            if (loop_cnt[i] > loop_cnt[i-1]):
-                axs[1].axvline(ns[i-1], linestyle='--', color='r')
-            elif (step_cnt[i] > step_cnt[i-1]):
-                axs[1].axvline(ns[i-1], linestyle='--', color='g')
-        plt_max = max([max(motor_vel[cc:]), -min(motor_vel[cc:])])*1.2
+            if (loop_cnt[i] > loop_cnt[i - 1]):
+                axs[1].axvline(ns[i - 1], linestyle='--', color='r')
+            elif (step_cnt[i] > step_cnt[i - 1]):
+                axs[1].axvline(ns[i - 1], linestyle='--', color='g')
+        plt_max = max([max(motor_vel[cc:]), -min(motor_vel[cc:])]) * 1.2
         axs[1].set_ylim(-plt_max, plt_max)
         axs[1].set_title('i_d=  10.0A @ 2Hz')
         axs[1].set_ylabel('motor_vel (rad/s)')
@@ -81,7 +98,7 @@ def phase_postprocess(plot_all=False):
         axs[1].spines['left'].set_visible(False)
         plt.show()
 
-    #Plot position ---------------------------------------------------------------
+    # Plot position ---------------------------------------------------------------
     if plot_all:
         fig, axs = plt.subplots(2)
         fig.suptitle('Full test position')
@@ -94,20 +111,19 @@ def phase_postprocess(plot_all=False):
         axs[0].grid(b=True, which='minor', axis='y', linestyle='--')
         axs[0].legend()
 
-        axs[0].set_ylim(min(link_pos)*0.8, max(link_pos)*1.2)
+        axs[0].set_ylim(min(link_pos) * 0.8, max(link_pos) * 1.2)
         axs[0].grid(b=True, which='major', axis='y', linestyle='-')
         axs[0].grid(b=True, which='minor', axis='y', linestyle=':')
-        axs[0].yaxis.set_major_locator(plt.MultipleLocator(
-            (max(motor_pos) - min(motor_pos))*1.2 / 4))
-        axs[0].yaxis.set_minor_locator(plt.MultipleLocator(
-            (max(motor_pos) - min(motor_pos))*1.2 / 12))
+        axs[0].yaxis.set_major_locator(
+            plt.MultipleLocator((max(motor_pos) - min(motor_pos)) * 1.2 / 4))
+        axs[0].yaxis.set_minor_locator(
+            plt.MultipleLocator((max(motor_pos) - min(motor_pos)) * 1.2 / 12))
         axs[0].spines['top'].set_visible(False)
         axs[0].spines['right'].set_visible(False)
         axs[0].spines['left'].set_visible(False)
 
-
         fig.suptitle('motor_pos - link_pos')
-        diff = [motor_pos[i]-link_pos[i] for i in range(0, len(motor_pos))]
+        diff = [motor_pos[i] - link_pos[i] for i in range(0, len(motor_pos))]
 
         axs[1].axhline(0, color='black', lw=1.2)
         axs[1].plot(ns, diff, label='diff', color='r', marker='.')
@@ -116,164 +132,203 @@ def phase_postprocess(plot_all=False):
         axs[1].grid(b=True, which='major', axis='y', linestyle='-')
         axs[1].grid(b=True, which='minor', axis='y', linestyle='--')
 
-        axs[1].set_ylim(min(diff)*1.2, max(diff)*1.2)
+        axs[1].set_ylim(min(diff) * 1.2, max(diff) * 1.2)
         axs[1].grid(b=True, which='major', axis='y', linestyle='-')
         axs[1].grid(b=True, which='minor', axis='y', linestyle=':')
         axs[1].yaxis.set_major_locator(
-            plt.MultipleLocator((max(diff) - min(diff))*1.2 / 4))
+            plt.MultipleLocator((max(diff) - min(diff)) * 1.2 / 4))
         axs[1].yaxis.set_minor_locator(
-            plt.MultipleLocator((max(diff) - min(diff))*1.2 / 12))
+            plt.MultipleLocator((max(diff) - min(diff)) * 1.2 / 12))
         axs[1].spines['top'].set_visible(False)
         axs[1].spines['right'].set_visible(False)
         axs[1].spines['left'].set_visible(False)
-        #axs.legend()
+        # axs.legend()
         plt.show()
 
     # split trajectories in individual motions -----------------------------------------------------
-    trj_v = [i for i in range(0,len(trj_cnt)) if (trj_cnt[i]==0 and curr_type[i] ==0)]
-    for i in range(trj_v[-1],len(curr_type)):
-        if curr_type[i] != curr_type[i-1]:
+    trj_v = [
+        i for i in range(0, len(trj_cnt))
+        if (trj_cnt[i] == 0 and curr_type[i] == 0)
+    ]
+    for i in range(trj_v[-1], len(curr_type)):
+        if curr_type[i] != curr_type[i - 1]:
             trj_v.append(i)
             break
 
-    phase = [ ph_angle[i] for i in range(0,len(ph_angle)-1) if (ph_angle[i] != ph_angle[i+1]) ]
+    phase = [
+        ph_angle[i] for i in range(0,
+                                   len(ph_angle) - 1)
+        if (ph_angle[i] != ph_angle[i + 1])
+    ]
     phase.append(ph_angle[-2])
 
-    ts = [[ s-ns[trj_v[j-1]] for s in ns[trj_v[j-1]:trj_v[j]] ] for j in range(1,len(trj_v)) ]
-    steps = [[ v for v in motor_vel[trj_v[j-1]:trj_v[j]] ] for j in range(1,len(trj_v)) ]
+    ts = [[s - ns[trj_v[j - 1]] for s in ns[trj_v[j - 1]:trj_v[j]]]
+          for j in range(1, len(trj_v))]
+    steps = [[v for v in motor_vel[trj_v[j - 1]:trj_v[j]]]
+             for j in range(1, len(trj_v))]
 
-    alpha=0.1
-    smooth=steps
-    for i in range(0,len(steps)):
-        for j in range(1,len(steps[i])):
-            smooth[i][j]=alpha*steps[i][j] + (1-alpha)*smooth[i][j-1]
+    alpha = 0.1
+    smooth = steps
+    for i in range(0, len(steps)):
+        for j in range(1, len(steps[i])):
+            smooth[i][j] = alpha * steps[i][j] + (1 - alpha) * smooth[i][j - 1]
 
-
-    #Plot individual trajectories ----------------------------------------------------------------------
+    # Plot individual trajectories ----------------------------------------------------------------------
     if plot_all:
         fig, axs = plt.subplots()
         fig.suptitle('Individual trajectories')
-        for i in range(0,len(ts)):
+        for i in range(0, len(ts)):
             axs.plot(ts[i], steps[i], label='diff')
             axs.plot(ts[i], smooth[i], label='diff')
 
         axs.set_ylabel('motor_vel (mrad/s)')
         axs.set_xlabel('timestamp (ns)')
 
-        axs.grid(b=True, which='major',axis='y', linestyle='-')
-        axs.grid(b=True, which='minor',axis='y', linestyle=':')
-        axs.yaxis.set_major_locator(plt.MultipleLocator((max(motor_vel) - min(motor_vel))*1.1 / 6))
-        axs.yaxis.set_minor_locator(plt.MultipleLocator((max(motor_vel) - min(motor_vel))*1.1 / 18))
+        axs.grid(b=True, which='major', axis='y', linestyle='-')
+        axs.grid(b=True, which='minor', axis='y', linestyle=':')
+        axs.yaxis.set_major_locator(
+            plt.MultipleLocator((max(motor_vel) - min(motor_vel)) * 1.1 / 6))
+        axs.yaxis.set_minor_locator(
+            plt.MultipleLocator((max(motor_vel) - min(motor_vel)) * 1.1 / 18))
         axs.spines['top'].set_visible(False)
         axs.spines['right'].set_visible(False)
         axs.spines['left'].set_visible(False)
         plt.show()
 
     # evaluate performance of each trajectory -----------------------------------------------------------
-    score=[[],[],[]]
-    for i in range(0,len(steps)):
+    score = [[], [], []]
+    for i in range(0, len(steps)):
         score[0].append(phase[i])
-        tmp = sum(steps[i][0:int(len(steps[i])/2)])
+        tmp = sum(steps[i][0:int(len(steps[i]) / 2)])
         if tmp > 0:
-            score[1].append(  (max(  steps[i][:]) - min(  steps[i][:])) /2 )
-            score[2].append(  (max( smooth[i][:]) - min( smooth[i][:])) /2 )
+            score[1].append((max(steps[i][:]) - min(steps[i][:])) / 2)
+            score[2].append((max(smooth[i][:]) - min(smooth[i][:])) / 2)
         else:
-            score[1].append( -(max(  steps[i][:]) - min(  steps[i][:])) /2 )
-            score[2].append( -(max( smooth[i][:]) - min( smooth[i][:])) /2 )
+            score[1].append(-(max(steps[i][:]) - min(steps[i][:])) / 2)
+            score[2].append(-(max(smooth[i][:]) - min(smooth[i][:])) / 2)
 
     REPEAT_COST = 3
     STEP_1 = 13
     STEP_2 = 13
-    BASE2 = STEP_1*REPEAT_COST
+    BASE2 = STEP_1 * REPEAT_COST
 
-    keys_1 = phase[:       STEP_1]
-    keys_2 = phase[BASE2: BASE2+STEP_2]
+    keys_1 = phase[:STEP_1]
+    keys_2 = phase[BASE2:BASE2 + STEP_2]
 
     _phase = []
     vals_1 = []
     for i in range(0, STEP_1):
         _phase.append(phase[i])
         i1 = score[1][i]
-        i2 = score[1][i+STEP_1]
-        i3 = score[1][i+STEP_1*2]
-        vals_1.append((i1+i2+i3)/3)
+        i2 = score[1][i + STEP_1]
+        i3 = score[1][i + STEP_1 * 2]
+        vals_1.append((i1 + i2 + i3) / 3)
 
     vals_2 = []
-    for i in range(BASE2, BASE2+STEP_2):
+    for i in range(BASE2, BASE2 + STEP_2):
         _phase.append(phase[i])
         i1 = score[1][i]
-        i2 = score[1][i+STEP_2]
-        i3 = score[1][i+STEP_2*2]
-        vals_2.append((i1+i2+i3)/3)
+        i2 = score[1][i + STEP_2]
+        i3 = score[1][i + STEP_2 * 2]
+        vals_2.append((i1 + i2 + i3) / 3)
 
-
-    #Fit 2nd order polinomial and plot max vs phase ------------------------------------------------------
+    # Fit 2nd order polinomial and plot max vs phase ------------------------------------------------------
     fig, axs = plt.subplots()
 
     keys_1.append(keys_1[0] + 2 * np.pi)
     vals_1.append(vals_1[0])
-    l1,= axs.plot(keys_1, vals_1, color='#1f77b4', marker='.', linestyle='--')
-    l2,= axs.plot(keys_2, vals_2, color='#ff7f0e', marker='.', linestyle='-')
+    l1, = axs.plot(keys_1, vals_1, color='#1f77b4', marker='.', linestyle='--')
+    l2, = axs.plot(keys_2, vals_2, color='#ff7f0e', marker='.', linestyle='-')
 
-    keys_1s=[v-2*np.pi for v in keys_1]
-    keys_2s=[v-2*np.pi for v in keys_2]
+    keys_1s = [v - 2 * np.pi for v in keys_1]
+    keys_2s = [v - 2 * np.pi for v in keys_2]
     axs.plot(keys_1s, vals_1, color='#1f77b4', marker='.', linestyle='--')
     axs.plot(keys_2s, vals_2, color='#ff7f0e', marker='.', linestyle='-')
-    
 
     z1 = np.polyfit(keys_2, vals_2, 2)
     z2 = np.polyfit(keys_2s, vals_2, 2)
     p1 = np.poly1d(z1)
     p2 = np.poly1d(z2)
-    x1 = np.linspace(p1.roots[0],p1.roots[1],1000)
-    x2 = np.linspace(p2.roots[0],p2.roots[1],1000)
+    x1 = np.linspace(p1.roots[0], p1.roots[1], 1000)
+    x2 = np.linspace(p2.roots[0], p2.roots[1], 1000)
 
-    l3, =axs.plot(x1,p1(x1),color='#2ca02c', linestyle=':')
-    axs.plot(x2,p2(x2),color='#2ca02c', linestyle=':')
+    l3, = axs.plot(x1, p1(x1), color='#2ca02c', linestyle=':')
+    axs.plot(x2, p2(x2), color='#2ca02c', linestyle=':')
 
-    #find best
+    # find best
     fit_angle = p1.deriv().roots[0]
-    if fit_angle <= 2*np.pi:
-        axs.set_xlim(0,2*np.pi)
-        l4, =axs.plot(fit_angle, p1(fit_angle), color='r', marker='x', linestyle='None')
+    if keys_2[0] <= 0:
+        axs.set_xlim(-np.pi, np.pi)
+        l4, = axs.plot(fit_angle,
+                       p1(fit_angle),
+                       color='r',
+                       marker='x',
+                       linestyle='None')
+
+    elif fit_angle > 2 * np.pi:
+        fit_angle = fit_angle - 2 * np.pi
+        axs.set_xlim(-np.pi, np.pi)
+        l4, = axs.plot(fit_angle,
+                       p2(fit_angle),
+                       color='r',
+                       marker='x',
+                       linestyle='None')
+
     else:
-        fit_angle = fit_angle -2*np.pi
-        axs.set_xlim(-np.pi,np.pi)
-        l4, =axs.plot(fit_angle, p2(fit_angle), color='r', marker='x', linestyle='None')
+        axs.set_xlim(0, 2 * np.pi)
+        l4, = axs.plot(fit_angle,
+                       p1(fit_angle),
+                       color='r',
+                       marker='x',
+                       linestyle='None')
 
     # make plot pretty
     fig.suptitle('Result: ph_angle = {:.3f}'.format(fit_angle))
     axs.set_ylabel('max velocity (mrad/s)')
     axs.set_xlabel('phase angle (rad)')
-    axs.grid(b=True, which='major',axis='x', linestyle=':')
-    axs.grid(b=True, which='major',axis='y', linestyle='-')
-    axs.grid(b=True, which='minor',axis='y', linestyle=':')
+    axs.grid(b=True, which='major', axis='x', linestyle=':')
+    axs.grid(b=True, which='major', axis='y', linestyle='-')
+    axs.grid(b=True, which='minor', axis='y', linestyle=':')
 
     axs.axvline(0, color='black', lw=1.2)
-    axs.set_ylim(min(vals_1)*1.1, max(vals_1)*1.1)
-    axs.yaxis.set_major_locator(plt.MultipleLocator((max(vals_1) - min(vals_1))*1.1 / 6))
-    axs.yaxis.set_minor_locator(plt.MultipleLocator((max(vals_1) - min(vals_1))*1.1 / 18))
+    axs.set_ylim(min(vals_1) * 1.1, max(vals_1) * 1.1)
+    axs.yaxis.set_major_locator(
+        plt.MultipleLocator((max(vals_1) - min(vals_1)) * 1.1 / 6))
+    axs.yaxis.set_minor_locator(
+        plt.MultipleLocator((max(vals_1) - min(vals_1)) * 1.1 / 18))
 
-    #axs.set_xlim(0,2*np.pi)
     axs.axhline(0, color='black', lw=1.2)
     axs.xaxis.set_major_locator(plt.MultipleLocator(np.pi / 4))
     axs.xaxis.set_minor_locator(plt.MultipleLocator(np.pi / 12))
-    axs.xaxis.set_major_formatter(plt.FuncFormatter(plot_utils.multiple_formatter(denominator=4,number=np.pi, latex='\pi')))
+    axs.xaxis.set_major_formatter(
+        plt.FuncFormatter(
+            plot_utils.multiple_formatter(denominator=4,
+                                          number=np.pi,
+                                          latex='\pi')))
 
     axs.spines['top'].set_visible(False)
     axs.spines['right'].set_visible(False)
     axs.spines['left'].set_visible(False)
-    axs.legend(handles=(l1,l2,l3,l4),labels=('Round 1','Round 2', 'Fitted curve','best ph_angle'))
+    axs.legend(handles=(l1, l2, l3, l4),
+               labels=('Round 1', 'Round 2', 'Fitted curve', 'best ph_angle'))
     if plot_all:
         plt.show()
 
     print('Result: ph_angle = ' + str(fit_angle))
-    
+
     # Save the picture ------------------------------------------------------------------------------------------------------
-    pdf_name = file[:-4]+'.pdf'
-    print('Saving graph as: '+pdf_name)
+    pdf_name = file[:-4] + '.pdf'
+    print('Saving graph as: ' + pdf_name)
     plt.savefig(fname=pdf_name, format='pdf')
+    print(bcolors.OKBLUE + "[i] Ending program" + bcolors.ENDC)
 
 
 if __name__ == "__main__":
+    print(bcolors.OKBLUE + "[i] Starting phase-calib-free" + bcolors.ENDC)
+    print('Starting phase-calib-free')
+    cmd = '~/ecat_dev/ec_master_app/build/examples/motor-calib/phase-calib-free/phase-calib-free ~/ecat_dev/ec_master_app/examples/motor-calib/config.yaml'
+    if os.system(cmd):
+        import sys
+        sys.exit(bcolors.FAIL + '[e] Error during phase-calib-free' + bcolors.ENDC)
+
     phase_postprocess(plot_all=False)
