4f6c2557b575408b407ce0dfeda352c6f5fc4ada
m-tartari <michele.tartari@iit.it>
2021-06-16 17:23:44 +0200

    🚩 refactor(all): use standalone ec_master_app
    follow changes from  ADVRHumanoids/ec_master_app@00fb74f
    see ADVRHumanoids/tree-organizational#691

--- motor_calibration.py
+++ motor_calibration.py
@@ -23,25 +23,25 @@ from utils.prompt_utils import single_yes_or_no_question as prompt_user
 
 ## Parameters:
 # path to test-pdo
-cmd0 = os.path.expanduser('~/ecat_dev/ec_master_app/build/examples/motor-calib/calib-test-pdo/calib-test-pdo')
+cmd0 = os.path.expanduser('~/ecat_dev_old/ec_master_app/build/examples/motor-calib/calib-test-pdo/calib-test-pdo')
 # path to phase-calib to test phase angle and log data
-cmd1 = os.path.expanduser('~/ecat_dev/ec_master_app/build/examples/motor-calib/phase-calib/phase-calib')
+cmd1 = os.path.expanduser('~/ecat_dev_old/ec_master_app/build/examples/motor-calib/phase-calib/phase-calib')
 # path to set-phase to set the optimized value to the motor
-cmd1b = os.path.expanduser('~/ecat_dev/ec_master_app/build/examples/motor-calib/set-phase/set-phase')
+cmd1b = os.path.expanduser('~/ecat_dev_old/ec_master_app/build/examples/motor-calib/set-phase/set-phase')
 # path to ripple-calib to test test ripple and positionl offset
-cmd2 = os.path.expanduser('~/ecat_dev/ec_master_app/build/examples/motor-calib/torque-calib/torque-calib')
+cmd2 = os.path.expanduser('~/ecat_dev_old/ec_master_app/build/examples/motor-calib/torque-calib/torque-calib')
 # path to set-phase to set the optimized value to the motor
-cmd2b = os.path.expanduser('~/ecat_dev/ec_master_app/build/examples/motor-calib/set-torque/set-torque')
+cmd2b = os.path.expanduser('~/ecat_dev_old/ec_master_app/build/examples/motor-calib/set-torque/set-torque')
 # path to ripple-calib to test test ripple and positionl offset
-cmd3 = os.path.expanduser('~/ecat_dev/ec_master_app/build/examples/motor-calib/ripple-calib/ripple-calib')
+cmd3 = os.path.expanduser('~/ecat_dev_old/ec_master_app/build/examples/motor-calib/ripple-calib/ripple-calib')
 # path to friction-calib for friction identification
-cmd4 = os.path.expanduser('~/ecat_dev/ec_master_app/build/examples/motor-calib/friction-calib/friction-calib')
+cmd4 = os.path.expanduser('~/ecat_dev_old/ec_master_app/build/examples/motor-calib/friction-calib/friction-calib')
 # path to inertia-calib for inertia identification
-cmd4b = os.path.expanduser('~/ecat_dev/ec_master_app/build/examples/motor-calib/inertia-calib/inertia-calib')
+cmd4b = os.path.expanduser('~/ecat_dev_old/ec_master_app/build/examples/motor-calib/inertia-calib/inertia-calib')
 # path to frequency-calib for frequecy response calibration
-cmd5 = os.path.expanduser('~/ecat_dev/ec_master_app/build/examples/motor-calib/frequency-calib/frequency-calib')
+cmd5 = os.path.expanduser('~/ecat_dev_old/ec_master_app/build/examples/motor-calib/frequency-calib/frequency-calib')
 # path to the configuration file for the motor and the test variables
-config_file = os.path.expanduser('~/ecat_dev/ec_master_app/examples/motor-calib/config.yaml')
+config_file = os.path.expanduser('~/ecat_dev_old/ec_master_app/examples/motor-calib/config.yaml')
 
 #print logo
 plot_utils.print_alberobotics()
